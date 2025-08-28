bacc0c436a3cbbdd40bb7629a42807a98cc783de
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2025-08-29 02:40:53 +0530

    validation for schema done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js")
 
 
 
@@ -35,6 +36,16 @@ app.get("/", (req, res) => {
     res.send(`Hi, I am root`);
 });
 
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
 // index route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
@@ -54,10 +65,7 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 // create route
-app.post("/listings", wrapAsync(async(req, res, next) => {
-    if(!req.body.listing) {
-        throw new ExpressError(400, "Please send valid data for listing");
-    }
+app.post("/listings", validateListing, wrapAsync(async(req, res, next) => {
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
@@ -72,7 +80,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 // update route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -116,8 +124,9 @@ app.all(/.*/, (req, res, next) => {
 // se usme char val enter nahi ki ja sakti:)
 app.use((err, req, res, next) => {
     let {statusCode = 500, message = "Something got Fu*ked up   "} = err;
-    res.status(statusCode).send(message);
-})
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
+});
 
 app.listen(port, () => {
     console.log(`server is on port ${port}`);
