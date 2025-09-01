7077f793a9c8f175e5a865837763d3db166d06d3
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2025-09-02 02:41:34 +0530

    implemented salting and hashing using passport (pbkdf2 hashing algorithm)

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 main()
@@ -54,12 +57,30 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session()); 
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     // console.log(res.locals.success);
     res.locals.error = req.flash("error");
     next();
 });
+
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    email: "student@gmail.com",
+    username: "fake-student",
+  });
+
+  let registeredUser = await User.register(fakeUser, "helloworld");
+  res.send(registeredUser);
+});
+
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
