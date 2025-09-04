92c98316822da900145cdb6c76a77b9cfe269aa2
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2025-09-04 11:44:08 +0530

    listed owner

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main(){
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "68b89fc17916da64cc959b2e"}));
     await Listing.insertMany(initData.data);
     console.log(`data was iniatialized`);
 };
