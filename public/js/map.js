be9f49b73d6fde6e84e27560fd23809ee8c61091
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2025-09-14 15:25:18 +0530

    add map marker and popups for it

--- public/js/map.js
+++ public/js/map.js
@@ -7,7 +7,18 @@
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    center: [79.0882, 21.1458], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 10 // starting zoom
+    container: 'map',
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 10, // starting zoom
+    style: 'mapbox://styles/mapbox/outdoors-v12'
 });
+
+// popup with HTML content
+const popup = new mapboxgl.Popup({ offset: 25 })
+  .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`);
+
+// marker and attach popup
+new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)  // Listing.geometry.coordinates
+  .setPopup(popup)
+  .addTo(map);
