46cbe31b0e563b31a491c343d95724d817f93f18
kaustubhk134 <kulurkarkaustubh134@gmail.com>
2025-09-14 14:08:22 +0530

    stored coordinated

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing.js");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding'); 
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -22,6 +26,15 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async(req, res, next) => {
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+    }) 
+    .send()
+
+    // console.log(response.body.features[0].geometry);
+    //res.send("done!");
+    
     let url = req.file.path;
     let filename = req.file.filename;
     // console.log(url, "..", filename);
@@ -29,7 +42,11 @@ module.exports.createListing = async(req, res, next) => {
     // console.log(req.user);
     newListing.owner = req.user._id;
     newListing.image = {url, filename};
-    await newListing.save();
+
+    newListing.geometry = response.body.features[0].geometry;
+
+    let savedListing = await newListing.save();
+    console.log(savedListing);
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 };
